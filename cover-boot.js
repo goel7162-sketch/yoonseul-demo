1e33a8aa4a9bb7d4d0b4a46641282b90fd11415c
goel7162-sketch <goel7162@gmail.com>
2026-08-19 04:06:40 +0900

    Make cover-boot.js ignore whitespace in base64 chunks

--- cover-boot.js
+++ cover-boot.js
@@ -4,7 +4,7 @@
   for (var i=1;i<=n;i++) files.push('cover-b64-'+i+'.txt');
   Promise.all(files.map(function(f){ return fetch(f,{cache:'no-store'}).then(function(r){ if(!r.ok) throw new Error(f+' '+r.status); return r.text(); }); }))
     .then(function(parts){
-      var url = 'data:image/jpeg;base64,' + parts.join('');
+      var url = 'data:image/jpeg;base64,' + parts.join('').replace(/\s+/g,'');
       function apply(root){
         (root||document).querySelectorAll('img[src="grandma-dog.jpg"]').forEach(function(img){ img.src = url; });
       }
